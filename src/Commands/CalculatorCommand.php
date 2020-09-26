c7788a2b94eb6230e0b027d7d91fb2ee81ab398c
Raffi Zidni <rafiimaut@gmail.com>
2020-09-26 15:31:23 +0700

    add command

---
+++ src/Commands/CalculatorCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Jakmall\Recruitment\Calculator\Commands;
+
+use Illuminate\Console\Command;
+
+abstract class CalculatorCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var string
+     */
+    protected $commandVerb;
+
+    /**
+     * @var string
+     */
+    protected $commandPassiveVerb;
+
+    /**
+     * @var string
+     */
+    protected $operator;
+
+    /**
+     * @param int|float $number1
+     * @param int|float $number2
+     *
+     * @return int|float
+     */
+    abstract function calculate($number1, $number2);
+
+    public function __construct()
+    {
+        $this->signature = $this->CommandSignature();
+        $this->description = $this->CommandDescription();
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        $numbers = $this->getInput();
+        $description = $this->generateCalculationDescription($numbers);
+        $result = $this->calculateAll($numbers);
+        $output = sprintf('%s = %s', $description, $result);
+
+        $this->comment($output);
+    }
+
+
+    protected function CommandSignature(): string
+    {
+        return sprintf(
+            '%s {numbers* : The numbers to be %s}',
+            $this->commandVerb,
+            $this->commandPassiveVerb
+        );
+    }
+
+    protected function CommandDescription(): string
+    {
+        return sprintf('%s all given Numbers', ucfirst($this->commandVerb));
+    }
+
+    protected function generateCalculationDescription(array $inputs): string
+    {
+        $glue = sprintf(' %s ', $this->operator);
+        return implode($glue, $inputs);
+    }
+
+    protected function getInput(): array
+    {
+        return $this->argument('numbers');
+    }
+
+    /**
+     * @param array $numbers
+     *
+     * @return float|int
+     */
+    protected function calculateAll(array $numbers)
+    {
+        $number = array_pop($numbers);
+
+        if (count($numbers) <= 0) {
+            return $number;
+        }
+
+        return $this->calculate($this->calculateAll($numbers), $number);
+    }
+}
